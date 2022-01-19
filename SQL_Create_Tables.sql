739e8494df60385a96ddcdc43c7f7bc924aa8010
Jay201733 <41226863+Jay201733@users.noreply.github.com>
2022-01-20 02:54:18 +0800

    Add files via upload
    Basics: Create Tables + Insert statements

---
+++ SQL_Create_Tables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE employeedemographics
+(employeeID int,
+firstname varchar(50),
+lastname varchar(50),
+age int,
+gender varchar(50)
+)
+
+CREATE TABLE employeesalary
+(employeeid int,
+jobtitle varchar(50),
+salary int
+)
+
+INSERT INTO employeedemographics VALUES
+(1001, 'jim', 'halpert', 30, 'male'),
+(1002, 'Pam', 'Beasley', 30, 'Female'),
+(1003, 'Dwight', 'Schrute', 29, 'Male'),
+(1004, 'Angela', 'Martin', 31, 'Female'),
+(1005, 'Toby', 'Flenderson', 32, 'Male'),
+(1006, 'Michael', 'Scott', 35, 'Male'),
+(1007, 'Meredith', 'Palmer', 32, 'Female'),
+(1008, 'Stanley', 'Hudson', 38, 'Male'),
+(1009, 'Kevin', 'Malone', 31, 'Male')
+
+INSERT INTO employeesalary VALUES
+(1001, 'Salesman', 45000),
+(1002, 'Receptionist', 36000),
+(1003, 'Salesman', 63000),
+(1004, 'Accountant', 47000),
+(1005, 'HR', 50000),
+(1006, 'Regional Manager', 65000),
+(1007, 'Supplier Relations', 41000),
+(1008, 'Salesman', 48000),
+(1009, 'Accountant', 42000)
